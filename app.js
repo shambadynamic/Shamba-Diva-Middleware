71f7f04a9c296ac6d5a99551d4e817f3dc231104
Syed Asad Kazmi <sak786@Syeds-MacBook-Air.local>
2023-03-01 20:56:51 +0530

    subgraph dependency removed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const oracleFacingContractAddress = "0xa86AC2eB05Fd8459eA07eC0cfD96722f4d75d02c"
 const dataProviderAddress = "0xbf405c325841545033920F59E9Aa9fe3d9c3c517"
 const divaDiamondAddress = "0xFf7d52432B19521276962B67FFB432eCcA609148"
 const divaDiamondABI = require("./diamondABI.json");
-const divaGraphUrl = "https://thegraph.com/hosted-service/subgraph/divaprotocol/diva-polygon-pilot"
+// const divaGraphUrl = "https://api.thegraph.com/subgraphs/name/divaprotocol/diva-polygon-pilot"
 
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
@@ -89,54 +89,94 @@ function getContractABI(contractAddress, apiKey) {
 //     }
 // }
 
+// function getEligiblePoolData() {
+
+//     timestampNow = Math.floor(Date.now() / 1000)
+
+//     timeStampBefore7Days = Math.floor(Date.now() / 1000) - (7 * 24 * 60 * 60)
+
+//     console.log(timestampNow, timeStampBefore7Days)
+
+//     const query = `{
+//         pools(orderBy: createdAt, where:{dataProvider: "${dataProviderAddress}", expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
+//         id
+//         referenceAsset
+//         expiryTime
+//         createdAt
+//         }
+//     }`
+
+//     return axios.post(divaGraphUrl, {
+//         'query': query
+//     })
+//         .then(async response => {
+//             console.log(response.data)
+
+//             while (true) {
+
+//                 lastId = response.data.data.pools[response.data.data.pools.length - 1].id
+
+//                 const queryMore = `{
+//                     pools(where:{id_gt: ${lastId}, dataProvider: "${dataProviderAddress}", expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
+//                     id
+//                     referenceAsset
+//                     expiryTime
+//                     createdAt
+//                     }
+//                 }`
+
+//                 newResponse = await axios.post(divaGraphUrl, {
+//                     'query': queryMore
+//                 })
+
+//                 if (newResponse.data.data.pools.length == 0) {
+//                     return response.data.data.pools
+//                 }
+//                 else {
+//                     response.data.data.pools = response.data.data.pools.concat(newResponse.data.data.pools)
+//                 }
+
+//             }
+
+//         }).catch(error => {
+//             console.error('There was an error!', error);
+//             return []
+//         })
+// }
+
 function getEligiblePoolData() {
+
+    poolId = 5
 
     timestampNow = Math.floor(Date.now() / 1000)
 
     timeStampBefore7Days = Math.floor(Date.now() / 1000) - (7 * 24 * 60 * 60)
 
-    const query = `{
-        pools(orderBy: createdAt, where:{dataProvider: ${dataProviderAddress}, expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
-        id
-        referenceAsset
-        expiryTime
+    const DivaDiamondContract = new ethers.Contract(
+        divaDiamondAddress,
+        divaDiamondABI,
+        provider
+    )
+
+    DivaDiamondContract.getPoolParameters(poolId).then(poolData => {
+
+        expiryTime = parseInt(poolData[13].toString())
+
+        createdAt = parseInt(poolData[7].toString())
+
+        dataProvider = poolData[14].toString()
+
+        referenceAsset = poolData[18].toString()
+
+        statusFinalReferenceValue = poolData[17]
+
+        if (dataProvider == dataProviderAddress && expiryTime < timestampNow && expiryTime >= timeStampBefore7Days && statusFinalReferenceValue == 0) {
+            return [{"id": poolId, "createdAt": createdAt, "expiryTime": expiryTime, "referenceAsset": referenceAsset}]
         }
-    }`
-
-    return axios.post(divaGraphUrl, {
-        'query': query
     })
-        .then(response => {
-            // console.log(response.data)
 
-            while (true) {
-
-                lastId = response.data.data.pools[response.data.data.pools.length - 1].id
-
-                const queryMore = `{
-                    pools(where:{id_gt: ${lastId}, dataProvider: ${dataProviderAddress}, expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
-                    id
-                    referenceAsset
-                    }
-                }`
-
-                newResponse = await axios.post(divaGraphUrl, {
-                    'query': queryMore
-                })
-
-                if (newResponse.data.data.pools.length == 0) {
-                    return response.data.data.pools
-                }
-                else {
-                    response.data.data.pools = response.data.data.pools.concat(newResponse.data.data.pools)
-                }
-
-            }
-
-        }).catch(error => {
-            console.error('There was an error!', error);
-            return []
-        })
+    return []
+  
 }
 
 function getRequestDataFromIPFS(ipfsUrl) {
@@ -267,7 +307,9 @@ function isValidRequest(requestData, poolExpiryTime, poolCreationTime) {
                 diffTime_Pool = Math.abs(pool_expiry_date - end_date)
                 diffDays_Pool = Math.ceil(diffTime_Pool / (1000 * 60 * 60 * 24))
 
-                if (diffDays >= 30 && diffDays_Pool >= 10 && start_date > pool_creation_date) {
+                if (diffDays >= 30) {
+
+                    //&& diffDays_Pool >= 10 && start_date > pool_creation_date
                     if (geometry != undefined && geometry.hasOwnProperty("features") && geometry.features.length > 0) {
 
                         var i = 0
