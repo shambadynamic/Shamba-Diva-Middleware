41062d2f1511b1ec70134281baa9a8937eca061b
Syed Asad Kazmi <sak786@Syeds-MacBook-Air.local>
2023-03-02 21:39:44 +0530

    pool_expiry_date and pool_creation_date conditions are uncommented, and upto 3 tries on oracle data reporting failure are implemented

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const provider = new ethers.providers.JsonRpcProvider(network_url);
 const signer = new ethers.Wallet(account_private_key, provider);
 const apiKey = process.env.POLYGONSCAN_API_KEY
 
-const oracleFacingContractAddress = "0xa86AC2eB05Fd8459eA07eC0cfD96722f4d75d02c"
+const oracleFacingContractAddress = "0xC324b5aeA9d4b8c5E13891633A36E75cc6c03b26"
 // const linkContractAddress = "0xb0897686c545045aFc77CF20eC7A532E3120E0F1"
 const dataProviderAddress = "0xbf405c325841545033920F59E9Aa9fe3d9c3c517"
 const divaDiamondAddress = "0xFf7d52432B19521276962B67FFB432eCcA609148"
 const divaDiamondABI = require("./diamondABI.json");
-// const divaGraphUrl = "https://api.thegraph.com/subgraphs/name/divaprotocol/diva-polygon-pilot"
+const divaGraphUrl = "https://api.thegraph.com/subgraphs/name/divaprotocol/diva-polygon-pilot"
 
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
@@ -89,101 +89,100 @@ function getContractABI(contractAddress, apiKey) {
 //     }
 // }
 
-// function getEligiblePoolData() {
-
-//     timestampNow = Math.floor(Date.now() / 1000)
-
-//     timeStampBefore7Days = Math.floor(Date.now() / 1000) - (7 * 24 * 60 * 60)
-
-//     console.log(timestampNow, timeStampBefore7Days)
-
-//     const query = `{
-//         pools(orderBy: createdAt, where:{dataProvider: "${dataProviderAddress}", expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
-//         id
-//         referenceAsset
-//         expiryTime
-//         createdAt
-//         }
-//     }`
-
-//     return axios.post(divaGraphUrl, {
-//         'query': query
-//     })
-//         .then(async response => {
-//             console.log(response.data)
-
-//             while (true) {
-
-//                 lastId = response.data.data.pools[response.data.data.pools.length - 1].id
-
-//                 const queryMore = `{
-//                     pools(where:{id_gt: ${lastId}, dataProvider: "${dataProviderAddress}", expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
-//                     id
-//                     referenceAsset
-//                     expiryTime
-//                     createdAt
-//                     }
-//                 }`
-
-//                 newResponse = await axios.post(divaGraphUrl, {
-//                     'query': queryMore
-//                 })
-
-//                 if (newResponse.data.data.pools.length == 0) {
-//                     return response.data.data.pools
-//                 }
-//                 else {
-//                     response.data.data.pools = response.data.data.pools.concat(newResponse.data.data.pools)
-//                 }
-
-//             }
-
-//         }).catch(error => {
-//             console.error('There was an error!', error);
-//             return []
-//         })
-// }
-
 function getEligiblePoolData() {
-
-    poolId = 5
 
     timestampNow = Math.floor(Date.now() / 1000)
 
     timeStampBefore7Days = Math.floor(Date.now() / 1000) - (7 * 24 * 60 * 60)
 
-    const DivaDiamondContract = new ethers.Contract(
-        divaDiamondAddress,
-        divaDiamondABI,
-        provider
-    )
+    console.log(timestampNow, timeStampBefore7Days)
 
-    DivaDiamondContract.getPoolParameters(poolId).then(poolData => {
-
-        expiryTime = parseInt(poolData[13].toString())
-
-        createdAt = parseInt(poolData[7].toString())
-
-        dataProvider = poolData[14].toString()
-
-        referenceAsset = poolData[18].toString()
-
-        statusFinalReferenceValue = poolData[17]
-
-        if (dataProvider == dataProviderAddress && expiryTime < timestampNow && expiryTime >= timeStampBefore7Days && statusFinalReferenceValue == 0) {
-            return [{"id": poolId, "createdAt": createdAt, "expiryTime": expiryTime, "referenceAsset": referenceAsset}]
+    const query = `{
+        pools(orderBy: createdAt, where:{dataProvider: "${dataProviderAddress}", expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
+        id
+        referenceAsset
+        expiryTime
+        createdAt
         }
-    })
+    }`
 
-    return []
-  
+    return axios.post(divaGraphUrl, {
+        'query': query
+    })
+        .then(async response => {
+            console.log(response.data)
+
+            while (true) {
+
+                lastId = response.data.data.pools[response.data.data.pools.length - 1].id
+
+                const queryMore = `{
+                    pools(where:{id_gt: ${lastId}, dataProvider: "${dataProviderAddress}", expiryTime_lt: ${timestampNow}, expiryTime_gte: ${timeStampBefore7Days}, statusFinalReferenceValue: "Open"}) {
+                    id
+                    referenceAsset
+                    expiryTime
+                    createdAt
+                    }
+                }`
+
+                newResponse = await axios.post(divaGraphUrl, {
+                    'query': queryMore
+                })
+
+                if (newResponse.data.data.pools.length == 0) {
+                    return response.data.data.pools
+                }
+                else {
+                    response.data.data.pools = response.data.data.pools.concat(newResponse.data.data.pools)
+                }
+
+            }
+
+        }).catch(error => {
+            console.error('There was an error!', error);
+            return []
+        })
 }
+
+// function getEligiblePoolData() {
+
+//     poolId = 5
+
+//     timestampNow = Math.floor(Date.now() / 1000)
+
+//     timeStampBefore7Days = Math.floor(Date.now() / 1000) - (7 * 24 * 60 * 60)
+
+//     const DivaDiamondContract = new ethers.Contract(
+//         divaDiamondAddress,
+//         divaDiamondABI,
+//         provider
+//     )
+
+//     return DivaDiamondContract.getPoolParameters(poolId).then(poolData => {
+
+//         expiryTime = parseInt(poolData[13].toString())
+
+//         createdAt = parseInt(poolData[7].toString())
+
+//         dataProvider = poolData[14].toString()
+
+//         referenceAsset = poolData[18].toString()
+
+//         statusFinalReferenceValue = poolData[17]
+
+
+//         if (dataProvider == dataProviderAddress && expiryTime < timestampNow && expiryTime >= timeStampBefore7Days && statusFinalReferenceValue == 0) {
+//             return [{"id": poolId, "createdAt": createdAt, "expiryTime": expiryTime, "referenceAsset": referenceAsset}]
+//         }
+//         return []
+//     })
+// }
 
 function getRequestDataFromIPFS(ipfsUrl) {
     return axios.get(ipfsUrl)
         .then(response => {
 
-            //console.log(response.data)
+            console.log(response.data)
 
             return { "data": response.data }
 
@@ -205,7 +204,11 @@ async function sendRequest(agg_x, dataset_code, selected_band, image_scale, star
             signer
         )
 
-        return GeoConsumerContract.requestGeostatsData(agg_x, dataset_code, selected_band, image_scale, start_date, end_date, geometry)
+        gasPrice = provider.getGasPrice()
+
+        gasLimit = GeoConsumerContract.estimateGas.requestGeostatsData(agg_x, dataset_code, selected_band, image_scale, start_date, end_date, geometry)
+
+        return GeoConsumerContract.requestGeostatsData(agg_x, dataset_code, selected_band, image_scale, start_date, end_date, geometry, {gasLimit: gasLimit, gasPrice: gasPrice})
             .then(_ => {
 
                 // console.log(data)
@@ -240,6 +243,10 @@ async function getGeostatsData(offset) {
         return GeoConsumerContract.getGeostatsData()
             .then(geostats_data => {
 
+                if (geostats_data.toString() == "-1" || geostats_data == 0) {
+                    return "-1"
+                }
+
                 const buffer = ethers.utils.parseUnits(offset.toString(), "ether")
                 return ethers.BigNumber.from(geostats_data).add(buffer).toString()   // Converting to String representation of BigNumber _hex (already in the 18-decimal integer representation)
             })
@@ -262,7 +269,11 @@ async function setFinalReferenceValue(poolId, geostatsData) {
         signer
     )
 
-    return DivaDiamondContract.setFinalReferenceValue(poolId, geostatsData, true)
+    gasPrice = provider.getGasPrice()
+
+    gasLimit = DivaDiamondContract.estimateGas.setFinalReferenceValue(poolId, geostatsData, true)
+
+    return DivaDiamondContract.setFinalReferenceValue(poolId, geostatsData, true, {gasLimit: gasLimit, gasPrice: gasPrice})
         .then(tx => {
 
             if (tx.hash != undefined) {
@@ -307,9 +318,8 @@ function isValidRequest(requestData, poolExpiryTime, poolCreationTime) {
                 diffTime_Pool = Math.abs(pool_expiry_date - end_date)
                 diffDays_Pool = Math.ceil(diffTime_Pool / (1000 * 60 * 60 * 24))
 
-                if (diffDays >= 30) {
+                if (diffDays >= 30 && diffDays_Pool >= 10 && start_date > pool_creation_date) {
 
-                    //&& diffDays_Pool >= 10 && start_date > pool_creation_date
                     if (geometry != undefined && geometry.hasOwnProperty("features") && geometry.features.length > 0) {
 
                         var i = 0
@@ -344,6 +354,8 @@ function isValidRequest(requestData, poolExpiryTime, poolCreationTime) {
 exports.shambaDivaMiddleware = async (event, context) => {
 
     poolDataList = await getEligiblePoolData()
+
+    console.log(poolDataList)
 
     for (var poolData of poolDataList) {
         requestData = await getRequestDataFromIPFS(poolData.referenceAsset)
@@ -427,6 +439,23 @@ exports.shambaDivaMiddleware = async (event, context) => {
                         geostats_data = await getGeostatsData(offset)
 
                         console.log(geostats_data)
+
+                        tries = 1
+
+                        while (geostatsData == "-1" && tries < 3) {
+
+                            console.log("Try: ", tries)
+
+                            request = await sendRequest(agg_x, dataset, band, scale, start_date, end_date, geometry_array)
+                            // console.log(request)
+
+                            if (request != undefined && request.Success != undefined) {
+                                sleep(60000).then(async _ => {
+                                    geostats_data = await getGeostatsData(offset)
+                                    tries += 1
+                                })
+                            }
+                        }
 
                         finalResult = await setFinalReferenceValue(parseInt(poolData.id), geostats_data)
 
